2709816dc27b9aebc77e9ab338c8972e639bdcca
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-03-26 18:50:21 -0300

    Finalizado.

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,30 +1,59 @@
-const Login = require("../models/loginModel")
+const Login = require('../models/LoginModel');
 
-exports.index = (req, res)=>{
-    res.render('login')
-}
+exports.index = (req, res) => {
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
+};
 
-exports.register = async function(req,res){
-    try{
+exports.register = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
 
-        const login =new Login(req.body)
-        await login.register()
-        res.send(login.errors)
-    
-        if( login.errors.length>0){
-            req.flash('erros', login.errors)
-            req.session.save(function(){
-            // return res.redirect('back')
-            })
-            return
-        }   
-        req.flash('success', 'deu certo caraio')
-        req.session.save(function(){
-        //    return res.redirect('back')
-        });
-        
-        return res.send(login.errors)
-    }catch(e){
-      return res.render('404')
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
+      });
+      return;
     }
-}
+
+    req.flash('success', 'Seu usuário foi criado com sucesso.');
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.login = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
+      });
+      return;
+    }
+
+    req.flash('success', 'Você entrou no sistema.');
+    req.session.user = login.user;
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+};
+
